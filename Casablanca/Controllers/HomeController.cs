3b530843bd492f2f89398df5da337e2058a946ea
Minh <hien.minh1996@gmail.com>
2018-12-07 22:01:25 +0100

    feat(Displaying a collab profile)

--- Casablanca/Controllers/HomeController.cs
+++ Casablanca/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Casablanca.Models;
+using Casablanca.Models.Database;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -65,6 +66,21 @@ namespace Casablanca.Controllers
             }
             */
 
+            return View(model);
+        }
+
+        public ActionResult CollaboratorView(int id)
+        {
+
+			//creer un modele qui contient un collabo et le filer a la vue
+			//le collabo qui a cet id, ca nous fera gagner du temps jpense.
+
+			Dal dal = new Dal();
+			//dal.CreateCollaborator("Minh", "Nguyen");
+			//dal.CreateCollaborator("Ming", "Ngoun");
+
+			Collaborator model = dal.GetCollaborators()[id];
+			
             return View(model);
         }
     }
