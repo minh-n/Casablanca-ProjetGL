dfbb7e3b4e78dfc7a27b15b35a166c207ea2a30d
Morgan F <morgan.feurte@gmail.com>
2019-01-02 15:03:33 +0100

    feat(ExpenseReport): created controller and index

---
+++ Casablanca/Controllers/ExpenseReportController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Casablanca.Controllers
+{
+    public class ExpenseReportController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
