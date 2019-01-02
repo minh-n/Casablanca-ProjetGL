4d0deac6320cb2bd61117059c9660b951fdc1c05
Morgan F <morgan.feurte@gmail.com>
2019-01-02 15:12:09 +0100

    feat(ER) : created class

---
+++ Casablanca/Models/ExpenseReport.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Casablanca.Models {
+    public class ExpenseReport {
+        public int id { get; set; }
+        public string month { get; set; }
+        public float totalCost { get; set; }
+        public string nbLines { get; set; }
+        public string status { get; set; } // TODO : Enum statut ?
+
+        public Collaborator collaborator { get; set; }
+    }
+}
