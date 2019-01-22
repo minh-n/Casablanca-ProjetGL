78db9ab98d8ca51279bac3d9f580d135496a7c39
Minh <hien.minh1996@gmail.com>
2019-01-23 00:28:51 +0100

    chore(Advance): created model

---
+++ Casablanca/Models/Advances/AdvanceLine.cs
@@ -0,0 +1,114 @@
+﻿using Casablanca.Models.Database;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+
+namespace Casablanca.Models.Advances {
+    public enum LineType {
+        RESTAURANT,
+        HOTEL,
+        TAXI,
+        ADVANCE,
+        OTHER
+    }
+
+    public enum Treatment {
+        NOT_TREATED,
+        CDS,
+        COMPTA
+    }
+
+    public class ExpenseLine {
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "mission")]
+        public virtual Mission Mission { get; set; }
+        [Display(Name = "validateur")]
+        public virtual string ChiefValidator { get; set; }
+        [Required]
+        public LineType Type { get; set; }
+        [Required]
+        [Display(Name = "description")]
+        public string Description { get; set; }
+        [Required]
+        [Display(Name = "montant du frais")]
+        public float Cost { get; set; }
+        [Required]
+        [Display(Name = "date")]
+        public DateTime Date { get; set; }
+        [Display(Name = "justificatif")]
+        public string Justificatory { get; set; }
+
+        // Validations 
+		public bool Validated { get; set; }         // Is validated by the CDS
+		public Treatment Treated { get; set; }      // Has been treated by the CDS or the compta
+		public bool FinalValidation { get; set; }   // Is validated by the compta
+
+        public ExpenseLine() {
+            Mission = null;
+            ChiefValidator = "";
+            Type = LineType.HOTEL;
+            Description = "";
+            Cost = 0;
+            Date = new DateTime();
+            Justificatory = "";
+        }
+
+		public ExpenseLine(Mission mission, LineType type, string chiefValidator, string description, float cost, DateTime date, string justificatory)
+		{
+			Mission = mission;
+			ChiefValidator = chiefValidator;
+			Type = type;
+			Description = description;
+			Cost = cost;
+			Date = date;
+			Justificatory = justificatory;
+        }
+
+		public ExpenseLine(Mission mission, LineType type, string description, float cost, DateTime date, string justificatory) {
+            this.Mission = mission;
+            this.Type = type;
+            this.Description = description;
+            this.Cost = cost;
+            this.Date = date;
+            this.Justificatory = justificatory;
+        }
+
+        public bool Equals(ExpenseLine other) {
+
+            return (this.Mission == other.Mission &&
+                    this.Type == other.Type &&
+                    this.Description == other.Description &&
+                    this.Cost == other.Cost &&
+                    this.Date == other.Date &&
+                    this.Justificatory == other.Justificatory);
+        }
+
+        public void ComputeValidator(Processing parent) {
+            Dal dal = new Dal();
+
+            switch (parent) {
+                case Processing.CLASSIC:
+                    ChiefValidator = dal.GetCollaborator(Mission.ChiefId).FirstName + " " + dal.GetCollaborator(Mission.ChiefId).LastName;
+                    break;
+                case Processing.COMPTA:
+                    ChiefValidator = "Service Comptabilité";
+                    break;
+                case Processing.FINANCIAL_DIRECTOR:
+                    ChiefValidator = "Directeur financier";
+                    break;
+                case Processing.CEO:
+                    ChiefValidator = "PDG";
+                    break;
+            }
+
+            //dal.SaveChanges();
+        }
+    }
+}
---
+++ Casablanca/Models/Advances/AdvanceReport.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+
+namespace Casablanca.Models.Advances {
+
+	// See the requirements specification doc 
+	//(Cahier des charges) for more info
+	public enum AdvanceStatus {
+        UNSENT,
+        APPROVED,
+        PENDING_APPROVAL_1,
+        PENDING_APPROVAL_2,
+        REFUSED
+    }
+
+    public enum Month {
+        JANUARY, FEBRUARY, MARCH, APRIL, MAY, JUNE, JULY, AUGUST, SEPTEMBER, OCTOBER, NOVEMBER, DECEMBER 
+    }
+
+    public enum Processing {
+        CLASSIC,
+        COMPTA,
+        FINANCIAL_DIRECTOR,
+        CEO
+    }
+
+    public class ExpenseReport {
+
+        [Key]
+        public int Id { get; set; }
+        public Month Month { get; set; }
+        public int Year { get; set; }
+        public float TotalCost { get; set; }
+        public int NbLines { get; set; }
+        public AdvanceStatus Status { get; set; }
+
+        public virtual Collaborator Collaborator { get; set; }
+        public virtual List<ExpenseLine> ExpenseLines { get; set; }
+
+        public Processing Treatment { get; set; }
+
+        public ExpenseReport() {
+        }
+
+        public ExpenseReport(Collaborator coll, Month month, int year) {
+            this.Month = month;
+            this.Year = year;
+            this.TotalCost = 0;
+            this.NbLines = 0;
+            this.Status = AdvanceStatus.UNSENT;
+            this.ExpenseLines = new List<ExpenseLine>();
+            this.Collaborator = coll;
+            ComputeTreatment();
+
+            coll.ExpenseReports.Add(this);
+        }
+
+		public ExpenseReport(Collaborator coll, Month month, int year, AdvanceStatus stat)
+		{
+			this.Month = month;
+			this.Year = year;
+			this.TotalCost = 0;
+			this.NbLines = 0;
+			this.Status = stat;
+			this.ExpenseLines = new List<ExpenseLine>();
+			this.Collaborator = coll;
+            ComputeTreatment();
+
+            coll.ExpenseReports.Add(this);
+		}
+
+		public void AddLine(ExpenseLine el) {
+            this.ExpenseLines.Add(el);
+            this.NbLines++;
+            this.TotalCost += el.Cost;
+        }
+
+        public void RemoveLine(ExpenseLine el) {
+            if(this.ExpenseLines.Remove(el)) {
+                this.NbLines--;
+                this.TotalCost -= el.Cost;
+            }
+            
+            if (this.ExpenseLines.Count == 0)
+                this.TotalCost = 0.0f;
+        }
+
+        public void ComputeTreatment() {
+            Service s = Collaborator.Service;
+            Roles role = Collaborator.Role;
+
+            switch(role) {
+                case Roles.USER:
+                    if(s.Name.Contains("Compta")) {
+                        // Coll compta => double val DF
+                        Treatment = Processing.FINANCIAL_DIRECTOR;
+                    }
+                    else {
+                        // Cas classique
+                        Debug.WriteLine("OMG");
+                        Treatment = Processing.CLASSIC;
+                    }
+                    break;
+                case Roles.CHIEF:
+                    if(s.Name.Contains("Compta")) {
+                        // CDS compta => double val PDG
+                        Debug.WriteLine("OOOOOOOOOOH");
+                        Treatment = Processing.CEO;
+                    }
+                    else if (s.Name.Contains("RH")) {
+                        // CDS RH => double val compta
+                        Treatment = Processing.COMPTA;
+                    }
+                    else if (s.Name.Contains("Direction")) {
+                        // PDG => double val DF
+                        Treatment = Processing.FINANCIAL_DIRECTOR;
+                    }
+                    else {
+                        Treatment = Processing.COMPTA;
+                    }
+                    break;
+            }
+        }
+    }
+}
